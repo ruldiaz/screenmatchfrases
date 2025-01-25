505e3e7e85136cb15cab492ba15b8b9964b64b04
ruldiaz <rulhdiaz@gmail.com>
2025-01-24 19:16:21 -0600

    commit

--- scripts/getDatos.js
+++ scripts/getDatos.js
@@ -1,5 +1,5 @@
 // URL base de la API
-const baseURL = 'http://localhost:8080';
+const baseURL = 'https://screenmatchfrasesbackend.onrender.com/';
 
 export default function getDatos(endpoint) {
     return fetch(`${baseURL}${endpoint}`)
